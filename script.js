5abbb93c61a03f646bd4247f3ffc742bdff61827
Jack Murphy <signal.space@protonmail.com>
2020-10-01 18:50:50 -0500

    jquery render function progress

--- script.js
+++ script.js
@@ -3,38 +3,52 @@ let isNow = false;
 
 $("#currentDay").text(date);
 
-console.log(date);
+console.log(`Current date :: ${date}`);
 
 let currentTime = moment().format("h  a");
 
-console.log(currentTime);
+console.log(`Current time :: ${currentTime}`);
 
 let hour = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
 class timeBlock {
   constructor(currentTime, hour) {
     this.hour = hour;
-    if(currentTime === hour) {
-        isNow = true;
+    if (currentTime == hour) {
+      isNow = true;
     }
-    this.isNow = isNow
+    this.isNow = isNow;
   }
 }
-for (let i = 0; i < hour.length; i++) {
-    const element = hour[i];
-    let tB = new timeBlock(currentTime, hour[i])
-    console.log(tB);
-    blockDrawer();
-    if(timeBlock.isNow) {
-        $(".row").addClass(".present");
-    } else if (Number(currentTime) > Number(hour[i])) {
-        $(".row").addClass(".past");
-    } else {
-        $(".row").addClass(".future");
-    }
+function blockDrawer(time) {
+  $(`.time-block`).append(
+    $("<div/>", { class: "row" })
+      .append(
+        $("<div/>", { class: "col-1, hour" }).append($("<span/>", { text: `${time}` }))
+      )
+      .append($('<input type="text"/>', { class: "col-10,textarea" }))
+      .append($("<button/>", { class: "col-1, saveBtn" }))
+      
+  );
+
+  console.log(`Drawer success!`);
 }
 
+for (let i = 0; i < hour.length; i++) {
+  let thisTime = hour[i];
 
-function blockDrawer() {
-    $("#row").appendTo("#body");
+  const tB = new timeBlock(currentTime, hour[i]);
+  console.log(tB);
+  blockDrawer(thisTime);
+  console.log(`This block is ${hour[i]}`);
+  if (tB.isNow) {
+    $(`.row`).addClass(".present");
+    console.log(`This block is now`);
+  } else if (parseInt(currentTime) > parseInt(hour[i])) {
+    $(`.row`).addClass(".past");
+    console.log(`This block is in the past`);
+  } else {
+    $(`.row`).addClass(".future");
+    console.log(`This block is in the future`);
+  }
 }
